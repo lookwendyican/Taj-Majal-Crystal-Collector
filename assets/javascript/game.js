831c0f888e6be37e82e0b5370253ddf542acd7d0
lookwendyican <wendyalers@gmail.com>
2019-07-07 16:47:09 -0400

    adding new Pseudo code to JS file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,34 @@
-// create variables for crystals worth
+$(document).ready(function(){
+   
+ 
+
+// creates variables for crystals worth
+var garnet = $("#garnet");
+var jade =  $("#jade");
+var lapis= $("#lapis");
+var onyx = $("#onyx");
 
 
-//create variables for wins and losses and announcement
+//Creates variables for Display on site
+var targetScore = $("#arget-score");
+var yourScore =  $("#user-score");
+var winsDisplay= $("#wins-score");
+var lossDisplay = $("#losses-score");
 
 
-create variables and functions to change the DOM
+//create variables and functions to change the DOM
+
+targetScore
+
+
+
+//Everytime we click a crystal it should add until we reach Target.  
+//if you reach target, you win!
+
+//If we go OverconstrainedError,  you loose round.  Start Over!
+
+//Reset round
+
+
+
+}); //last tags for the READY function

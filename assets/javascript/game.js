235fc3900e03a21de6a0ad216e328edf0c26ae0e
lookwendyican <wendyalers@gmail.com>
2019-07-18 19:02:01 -0400

    reworking jquery

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,34 +1,57 @@
-$(document).ready(function(){
-   
- 
+$( document ).ready(function() {
+  
+//Setting up global variables
 
-// creates variables for crystals worth
-var garnet = $("#garnet");
-var jade =  $("#jade");
-var lapis= $("#lapis");
-var onyx = $("#onyx");
-
-
-//Creates variables for Display on site
-var targetScore = $("#arget-score");
-var yourScore =  $("#user-score");
-var winsDisplay= $("#wins-score");
-var lossDisplay = $("#losses-score");
+var randomResult;
+var wins =0;
+var losses = 0;
+var yourTotal = 0;
 
 
-//create variables and functions to change the DOM
+//Setting up random result for target number to try to match
 
-targetScore
-
-
-
-//Everytime we click a crystal it should add until we reach Target.  
-//if you reach target, you win!
-
-//If we go OverconstrainedError,  you loose round.  Start Over!
-
-//Reset round
+randomResult = Math.floor(Math.random() * 60) + 59;
+console.log(randomResult);
 
 
+//Setting up crystals with random numbers between 1-12 for each.
+$('#target-number').html(randomResult)
+for ( i = 0; i < 4; i++){
+    var randomCrystals = Math.floor(Math.random() * 11) + 1;
+    console.log(randomCrystals);
 
-}); //last tags for the READY function
+    //Setting up divs for crystals and assigning classes and random number attribute
+    var crystal = $("<div>");
+    crystal.attr({
+        "class": 'crystal',
+        "data-random": randomCrystals
+    });
+    $(".crystals").append(crystal);
+}
+
+//Function to get value from each crystal clicked.  
+$(".crystal").on('click', function(){
+    
+    var num = parseInt($(this).attr('data-random'));
+    //and add to each other with each click
+    yourTotal += num;
+    console.log(yourTotal);
+
+    //Alert user when they go over or if they won
+    if (yourTotal > randomResult){
+        losses--;
+        alert('You lose!  Try again!');
+    } else if  (yourTotal === randomResult){
+        wins++;
+        alert('You won! YEAH!')
+    }
+    
+
+  
+});
+
+    
+    
+      
+    }); //End Document ready 
+    

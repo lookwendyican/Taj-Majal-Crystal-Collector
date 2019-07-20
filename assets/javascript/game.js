a13114dff3b3907dea3ed908a9789e7e9628293d
lookwendyican <wendyalers@gmail.com>
2019-07-20 09:01:05 -0400

    finalized images and css and added comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,7 @@
 $( document ).ready(function() {
   
+
+    
 //Setting up global variables
 
 var randomResult;
@@ -7,12 +9,15 @@ var wins =0;
 var losses = 0;
 var yourTotal = 0;
 
+var images = [ "assets/images/transparent-garnet.png", "assets/images/green-transparent-stone-8.png" , "assets/images/transparent-lapis.png" , "assets/images/obsidian-transparent.png"]
 
 //Setting up random result for target number to try to match
 
 var startGame = function() {
 
     $(".crystals").empty();
+
+ 
     randomResult = Math.floor(Math.random() * 60) + 59;
     console.log(randomResult);
 
@@ -23,15 +28,17 @@ var startGame = function() {
         var randomCrystals = Math.floor(Math.random() * 11) + 1;
         console.log(randomCrystals);
 
-        //Setting up divs for crystals and assigning classes and random number attribute
-        var crystal = $("<div>");
+        //Setting up and IMAGE container, instead of a div to pull up cyrstals from array and assigning classes and random number attribute
+        var crystal = $("<img>");
         crystal.attr({
             "class": 'crystal',
-            "data-random": randomCrystals
+            "data-random": randomCrystals,
+            "src": images[i]
         });
-        crystal.html(randomCrystals);
+       
         $(".crystals").append(crystal);
     }
+    $("#user-total").html(yourTotal);
 } //End startGame function
 
 startGame ();
@@ -39,6 +46,8 @@ startGame ();
 
 
 //Function to get value from each crystal clicked.  
+
+//(Event delegation)
 $(document).on('click', '.crystal', function(){
     
     var num = parseInt($(this).attr('data-random'));
@@ -54,7 +63,7 @@ $(document).on('click', '.crystal', function(){
         //Add losses to counter
         $("#losses-score").html(losses);
         alert('You lose!  Try again!');
-        yourTotal = 0;
+        yourTotal =0;
         startGame ();
 
     } else if  (yourTotal === randomResult){
@@ -62,7 +71,7 @@ $(document).on('click', '.crystal', function(){
          //Add wins to counter
         $("#wins-score").html(wins);
         alert('You won! YEAH!')
-        yourTotal = 0;
+        yourTotal =0;
         startGame ();
     }
     
